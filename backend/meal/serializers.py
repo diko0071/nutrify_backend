3bb1f58c4ca4f778654978e4932ab25c270c194a
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-07-08 05:42:23 +0500

    mock base api for meal / meal iterms

--- backend/meal/serializers.py
+++ backend/meal/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import MealItem
+from .models import MealItem, Meal
 
 class MealItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MealItem
+        fields = '__all__'
+
+
+class MealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meal
         fields = '__all__'
--- backend/meal/views.py
+++ backend/meal/views.py
@@ -6,16 +6,44 @@ from dotenv import load_dotenv
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
-from .serializers import MealItemSerializer
-from .models import MealItem
+from .serializers import MealItemSerializer, MealSerializer
+from .models import MealItem, Meal
 from datetime import datetime, timedelta
 import json
 
 
 
+# Get meal items for a meal and all meals for day
 @api_view(['GET'])
-@permission_classes([AllowAny])
-def get_all_meal_items(request):
-    meal_items = MealItem.objects.all()
+def get_meal_items_for_meal(request, meal_id):
+    user = request.user
+    meal_items = MealItem.objects.filter(meal_id=meal_id, user=user)
     serializer = MealItemSerializer(meal_items, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def get_meal_by_day(request):
+    user = request.user
+    day = request.query_params.get('day')
+    meal = Meal.objects.get(day=day, user=user)
+    serializer = MealSerializer(meal)
+    return Response(serializer.data)
+
+# Interact with meal items
+@api_view(['POST'])
+def create_meal_item(request):
+    ...
+
+@api_view(['POST'])
+def create_meal_item_ai(request):
+    ...
+
+@api_view(['PUT'])
+def update_meal_item(request):
+    ...
+
+@api_view(['DELETE'])
+def delete_meal_item(request):
+    ...
+
+
